06e7aace09177091d385a108b746baa8abcc2d04
Troy Center <troycenter1@gmail.com>
2017-07-12 20:02:14 -0700

    app is working up to populating the comments. Next: populate comments and app is done.

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 ///////////////////////////////////////////////////
 //   Main app.js file for Coding Dojo Message board app. 
+// by: Troy Center, Coding Dojo, July 2017
 ///////////////////////////////////////////////////
-
-
+//
 ///////////////////////////////////////////////////
 //          Required programs
 ///////////////////////////////////////////////////
@@ -20,39 +20,89 @@ const app = express();
 //              Setup App variables 
 ///////////////////////////////////////////////////
 
-app.use(express.static(path.join(__dirname + '/static/')))
-app.set('views', path.join(__dirname + '/views/'))
+app.use(express.static(path.join(__dirname + '/static/')));
+app.set('views', path.join(__dirname + '/views/'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
 ///////////////////////////////////////////////////
-//              Setup Mongo DB
+//   Setup Mongo DB with Messages <<1/Many>> Comments
 ///////////////////////////////////////////////////
-mongoose.connect('mongodb://localhost/quoting_dojo');
+mongoose.connect('mongodb://localhost/message_board');
 mongoose.Promise = global.Promise;
 
+var Schema = mongoose.Schema;
+
 var MessageSchema = new mongoose.Schema({
-    name: String, 
-    message: String,
+    name: { type: String, required: true, minlength: 4 }, 
+    message: { type: String, required: true }, 
+    comments: [{type: Schema.Types.ObjectId, ref: 'Comment'}]
 }, { timestamps: true });
 
 mongoose.model('Message', MessageSchema);
 var Message = mongoose.model('Message');
 
+var CommentSchema = new mongoose.Schema({
+    // since this is a reference to a different document, the _ is the naming convention!
+    _message: {type: Schema.Types.ObjectId, ref: 'Message'},
+    name: { type: String, required: true },
+    comment: { type: String, required: true }, 
+    }, { timestamps: true });
+
+mongoose.model('Comment', CommentSchema);
+var Comment = mongoose.model('Comment');
+
 ///////////////////////////////////////////////////
 //                  Routes
 ///////////////////////////////////////////////////
 app.get('/', function (req, res){
-    res.render('index');
+    console.log("*****************************************************");
+    console.log("In app.js ROOT Route");
+    console.log("*****************************************************");
+    Message.find({}, function(err, messages){
+        if(err){
+            console.log("Error retrieving data from db... ", err);
+            res.redirect('/');
+        } else {
+            console.log("Success getting Messages from db: ", messages);
+            res.render('index', {messages: messages});
+        }
+    });
 });
 
 app.post('/newmessage', function(req, res){
-    console.log("*****************************************************")
-    console.log("In NewMessage app.js | req.body: ",req.body)
-    console.log("*****************************************************")
-    res.redirect('/');
-})
+    const message = new Message({name: req.body.name, message: req.body.message});
+    message.save(function(err){
+        if(err){ 
+            console.log('Error inserting into db... ', err);
+        } else {
+            console.log('Success: inserted message into db...');
+        }
+        res.redirect('/');
+    });
+});
 
+app.post('/newcomment', function(req, res){
+    Message.findOne({_id: req.body.messageid}, function(err, message){
+        if(err){
+            console.log("Could not find message in db, error... ");
+            res.redirect('/');
+        } else {
+            const comment = new Comment({name: req.body.name, comment: req.body.comment, _message: req.body.messageid});
+            message.comments.push(req.body.messageid);
+            comment.save(function(err){
+                message.save(function(err){
+                        if(err){ 
+                            console.log('Error inserting message or comment into db... ', err);
+                        } else {
+                            console.log('Success: inserted comment into db...');
+                        }
+                        res.redirect('/');
+                });
+            });
+        }
+    });
+});
 ///////////////////////////////////////////////////
 //          Listener on port 8000
 ///////////////////////////////////////////////////
